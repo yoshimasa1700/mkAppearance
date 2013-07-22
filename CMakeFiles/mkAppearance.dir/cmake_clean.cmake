f01ea6a48d042a7697b928c6e2807f7bf4e86fec
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2013-07-22 10:40:48 +0900

    first commit

---
+++ CMakeFiles/mkAppearance.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mkAppearance.dir/main.cpp.o"
+  "mkAppearance.pdb"
+  "mkAppearance"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mkAppearance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
